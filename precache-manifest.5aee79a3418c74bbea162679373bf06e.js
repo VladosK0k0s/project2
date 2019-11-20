547d3bad25893d98edd7851a737d90c051d75662
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-11-20 02:18:16 +0200

    Updates

---
+++ precache-manifest.5aee79a3418c74bbea162679373bf06e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be34a9e117f8ba33d38cb6b3ec3eae9a",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "ff13f24802af03e30474",
+    "url": "/project2/static/css/main.21803602.chunk.css"
+  },
+  {
+    "revision": "0d26e336389d8d9bbdf5",
+    "url": "/project2/static/js/2.57ebce8f.chunk.js"
+  },
+  {
+    "revision": "ff13f24802af03e30474",
+    "url": "/project2/static/js/main.732a1a06.chunk.js"
+  },
+  {
+    "revision": "0aba200b2cea97ca3482",
+    "url": "/project2/static/js/runtime-main.d98f9177.js"
+  }
+]);
